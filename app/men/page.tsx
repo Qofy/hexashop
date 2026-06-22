1c1f14fa7933cc75d21c6e984a9ea3164541de89
Qofy <safopencilart@icloud.com>
2026-06-22 06:11:37 +0200

    -adding men component for featured,latest,casual and trending

--- app/men/page.tsx
+++ app/men/page.tsx
@@ -1,7 +1,15 @@
+import MenProducts from "@/component/men/MenProducts"
+
 // type Props = {}
 
 export default function Men() {
   return (
-    <div>Men</div>
+    <div>
+      <MenProducts category="latest" title="Men's Latest"/>
+      <MenProducts category="featured" title="Futured"/>
+      <MenProducts category="casual" title="Casual"/>
+      <MenProducts category="trending" title="Men's Treding"/>
+
+    </div>
   )
 };
